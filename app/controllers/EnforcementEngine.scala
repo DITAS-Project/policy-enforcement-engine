3af4187b5f83bb46ca62f5bd72671c593707b767
Revital Sur <eres@il.ibm.com>
2018-07-31 13:37:26 +0300

    Test build phase 1

--- app/controllers/EnforcementEngine.scala
+++ app/controllers/EnforcementEngine.scala
@@ -5,9 +5,8 @@ import javax.inject.Inject
 import io.swagger.annotations._
 import models.{RequestQuery, ResponseQuery}
 import play.api.mvc._
-import play.api.libs.json.{Format, JsValue, Json}
+import play.api.libs.json.Json
 import play.api.{Configuration, Logger}
-import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
@@ -26,10 +25,15 @@ class EnforcementEngine @Inject() (config: Configuration) extends InjectedContro
     val queryObject = request.body
     println (queryObject.asText)
     var configPath: String = null
-    var result: Boolean = false
+    var result: Boolean = false;
     if (config.has("enforcmentEngine.preprocessing.configPath")) {
       configPath = config.get[String]("enforcmentEngine.preprocessing.configPath")
-      result = com.ibm.research.storage.policy.enforcement.preprocessing.spark.PreProcessing.doPreprocessing(configPath)
+      try {
+        //result = com.ibm.research.storage.policy.enforcement.preprocessing.spark.PreProcessing.doPreprocessing(configPath)
+        result = true
+      } catch {
+        case e: Exception => result = false
+      }
       if (result == true)
         Future.successful(Ok("Pre-processing phase finished successfully"))
       else {
@@ -64,17 +68,25 @@ class EnforcementEngine @Inject() (config: Configuration) extends InjectedContro
     val purpose = queryObject.purpose
     val requesterId = queryObject.requesterId
     var configPath:String = null
-    var result: String = "Missing config Parameter"
+
+    var result: String = ""
     if (config.has("enforcmentEngine.runtime.configPath")) {
       configPath = config.get[String]("enforcmentEngine.runtime.configPath")
       val accessType = queryObject.access
       val blueprintId = queryObject.blueprintId
       Logger.info(s"Received query: $query, for purpose: $purpose, blueprintId: $blueprintId")
-      result = com.ibm.research.storage.policy.enforcement.sql.runtime.SparkSqlRuntime.getCompliantResult(configPath,
-        purpose, accessType, query, "requester.id", requesterId)
-      if (result.equals("")) {
+      //lazy
+      if (config.has("enforcmentEngine.runtime.configPath"))
+        try{
+          //result = com.ibm.research.storage.policy.enforcement.sql.runtime.SparkSqlRuntime.getCompliantResult(configPath,purpose, accessType, query, "requester.id", requesterId)
+          result = "OK"
+        }catch {
+          case e: Exception => result = ""
+        }
+
+      if (result.equals(""))
         Future.successful(BadRequest("run time phase failed"))
-      } else {
+      else {
         //Future.successful(Ok(Json.toJson(response)))
         Future.successful(Ok(Json.toJson(result)))
       }
