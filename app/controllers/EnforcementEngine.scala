09020c6f994e38545aab2fb60b872f76632f4e82
Revital1 Eres <eres@iris-tes-cloud.sl.cloud9.ibm.com>
2018-09-20 14:53:05 +0300

    Remove doPreprocessing

--- app/controllers/EnforcementEngine.scala
+++ app/controllers/EnforcementEngine.scala
@@ -17,39 +17,6 @@ import scala.concurrent.Future
 @Api("Enforcement Engine")
 class EnforcementEngine @Inject() (config: Configuration,  initService: Init) extends InjectedController {
 
-  @ApiOperation(nickname = "doPreProcessing",
-    value = "do pre-processing phase of enforcement engine",
-    notes = "",
-    response = classOf[models.ResponseQuery], responseContainer = "List", httpMethod = "POST")
-  @ApiResponses(Array(
-    new ApiResponse(code = 400, message = "Invalid parameters supplied"),
-    new ApiResponse(code = 404, message = "config was not found")))
-  def doPreProcessing = Action.async { request =>
-    val queryObject = request.body
-    println (queryObject.asText)
-    var configFullPath: String = null
-    var result: Boolean = false;
-    var currentPath = Paths.get(".").toAbsolutePath+"config/"
-    if (config != null && config.has("enforcmentEngine.preprocessing.configFullPath")) {
-      configFullPath = config.get[String]("enforcmentEngine.preprocessing.configFullPath")
-      try {
-        result = com.ibm.research.storage.policy.enforcement.preprocessing.spark.PreProcessing.doPreprocessing(configFullPath)
-        //result = true
-      } catch {
-        case e: Exception => result = false
-      }
-      if (result == true)
-        Future.successful(Ok("Pre-processing phase finished successfully"))
-      else {
-        Future.successful(BadRequest("Pre-processing phase failed"))
-      }
-    } else {
-      Future.successful(NotFound("Missing config file"))
-    }
-  //  Future.successful(Ok("Pre-processing phase finished successfully"))
-  }
-
-
   @ApiOperation(nickname = "getCompliantDataResult",
     value = "Get Compliant result with enforcement options",
     notes = "",
@@ -131,7 +98,7 @@ class EnforcementEngine @Inject() (config: Configuration,  initService: Init) ex
       if (result.equals(""))
         Future.successful(BadRequest("run time phase failed"))
       else {
-        println (result)
+//        println (result)
         //Future.successful(Ok(Json.toJson(result)))
         Future.successful(Ok(result))
       }
