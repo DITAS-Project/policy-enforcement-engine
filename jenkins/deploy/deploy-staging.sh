a4fee9029ae6340d0b34a803e314fd8230936622
Revital1 Eres <eres@iris-tes-cloud.sl.cloud9.ibm.com>
2018-09-23 07:37:24 +0300

    Fix path on cloudSigma in deploy-staging

--- jenkins/deploy/deploy-staging.sh
+++ jenkins/deploy/deploy-staging.sh
@@ -4,10 +4,10 @@
 
 # TODO state management? We are killing without careing about any operation the conainer could be doing.
 
-scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/policy-enforcement-engine/connections-DITAS.yml cloudsigma@31.171.247.162:connections-DITAS.yml
-scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/policy-enforcement-engine/applicationEngine.conf cloudsigma@31.171.247.162:applicationEngine.conf
-scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/policy-enforcement-engine/demo-dpcm-DITAS.yml cloudsigma@31.171.247.162:demo-dpcm-DITAS.yml
-scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/policy-enforcement-engine/demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar cloudsigma@31.171.247.162:demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar
+scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/ehealth-sample-spark-vdc/connections-DITAS.yml cloudsigma@31.171.247.162:connections-DITAS.yml
+scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/ehealth-sample-spark-vdc/applicationEngine.conf cloudsigma@31.171.247.162:applicationEngine.conf
+scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/ehealth-sample-spark-vdc/demo-dpcm-DITAS.yml cloudsigma@31.171.247.162:demo-dpcm-DITAS.yml
+scp -i  /opt/keypairs/ditas-testbed-keypair.pem  /home/cloudsigma/configurations/ehealth-sample-spark-vdc/demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar cloudsigma@31.171.247.162:demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar
 ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
