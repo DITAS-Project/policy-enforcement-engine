60842254ed9173af8db24a9719ca524d35ac1667
Revital1 Eres <eres@iris-tes-cloud.sl.cloud9.ibm.com>
2018-09-25 17:25:43 +0300

    Fixes to deploy-staging.sh

--- jenkins/deploy/deploy-staging.sh
+++ jenkins/deploy/deploy-staging.sh
@@ -14,10 +14,10 @@ ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'END
 sudo docker rm -f policy-enforcement-engine || true
 sudo docker pull ditas/policy-enforcement-engine:latest
 sudo docker run -p 50004:9000 -d  -ti --entrypoint=sh --name policy-enforcement-engine ditas/policy-enforcement-engine:latest
-sudo docker rm /app/policy-enforcement-engine-1.0-SNAPSHOT/lib/dummy_demosparksql_2.11-0.1.jar
-sudo docker cp applicationEngine.conf  /app/policy-enforcement-engine-1.0-SNAPSHOT/conf/applicationEngine.conf
-sudo docker cp connections-DITAS.yml /app/policy-enforcement-engine-1.0-SNAPSHOT/conf/connections-DITAS.yml
-sudo docker cp demo-dpcm-DITAS.yml  /app/policy-enforcement-engine-1.0-SNAPSHOT/conf/demo-dpcm-DITAS.yml
-sudo docker cp demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar /app/policy-enforcement-engine-1.0-SNAPSHOT/lib/demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar
+sudo docker exec policy-enforcement-engine rm -rf /app/policy-enforcement-engine-1.0-SNAPSHOT/lib/dummy_demosparksql_2.11-0.1.jar 
+sudo docker cp applicationEngine.conf  policy-enforcement-engine:/app/policy-enforcement-engine-1.0-SNAPSHOT/conf/applicationEngine.conf
+sudo docker cp connections-DITAS.yml policy-enforcement-engine:/app/policy-enforcement-engine-1.0-SNAPSHOT/conf/connections-DITAS.yml
+sudo docker cp demo-dpcm-DITAS.yml  policy-enforcement-engine:/app/policy-enforcement-engine-1.0-SNAPSHOT/conf/demo-dpcm-DITAS.yml
+sudo docker cp demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar policy-enforcement-engine:/app/policy-enforcement-engine-1.0-SNAPSHOT/lib/demo-sparksql-1.0-SNAPSHOT-jar-with-dependencies.jar
 sudo docker exec -d -i policy-enforcement-engine /app/policy-enforcement-engine-1.0-SNAPSHOT/bin/policy-enforcement-engine -Dplay.http.secret.key='wspl4r' -Dconfig.file='app/policy-enforcement-engine/conf/applicationEngine.conf'
 ENDSSH
