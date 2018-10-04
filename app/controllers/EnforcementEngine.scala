de61a669105ee9731e2c1c7a1c56f8a938de521e
Revital1 Eres <eres@iris-tes-cloud.sl.cloud9.ibm.com>
2018-10-04 13:43:14 +0300

    Remove getCompliantDataResult and change return codes in case of error

--- app/controllers/EnforcementEngine.scala
+++ app/controllers/EnforcementEngine.scala
@@ -8,7 +8,7 @@ import play.api.mvc._
 import play.api.libs.json.Json
 import play.api.{Configuration, Logger}
 import java.nio.file.Paths
-
+import org.slf4j.LoggerFactory
 import bootstrap.Init
 
 import scala.concurrent.Future
@@ -16,58 +16,10 @@ import scala.concurrent.Future
 // TODO thread pool!!!
 @Api("Enforcement Engine")
 class EnforcementEngine @Inject() (config: Configuration,  initService: Init) extends InjectedController {
-
-  @ApiOperation(nickname = "getCompliantDataResult",
-    value = "Get Compliant result with enforcement options",
-    notes = "",
-    response = classOf[models.ResponseQuery], responseContainer = "List", httpMethod = "POST")
-  @ApiImplicitParams(Array(
-    new ApiImplicitParam(
-      value = "Request query",
-      required = true,
-      dataType = "models.RequestQuery",
-      paramType = "body"
-    )
-  ))
+  private val LOGGER = LoggerFactory.getLogger("EHealthVDCController")
   @ApiResponses(Array(
     new ApiResponse(code = 400, message = "Invalid parameters supplied"),
-    new ApiResponse(code = 404, message = "Access purpose not found")))
-  def getCompliantDataResult = Action.async(parse.json[RequestQuery]) { request =>
-
-    val queryObject = request.body
-    val query = queryObject.query
-    val purpose = queryObject.purpose
-    val requesterId = queryObject.requesterId
-    var configFullPath:String = null
-
-    var result: String = ""
-
-    if (config != null && config.has("enforcmentEngine.runtime.configFullPath")) {
-      configFullPath = config.get[String]("enforcmentEngine.runtime.configFullPath")
-      val accessType = queryObject.access
-      val blueprintId = queryObject.blueprintId
-      Logger.info(s"Received query: $query, for purpose: $purpose, blueprintId: $blueprintId")
-      //lazy
-      if (config.has("enforcmentEngine.runtime.configFullPath"))
-        try{
-          result = com.ibm.research.storage.policy.enforcement.sql.runtime.SparkSqlRuntime.getCompliantResult(configFullPath,purpose, accessType, query, "requester.id", requesterId)
-        }catch {
-          case e: Exception => result = ""
-        }
-
-      if (result.equals(""))
-        Future.successful(BadRequest("run time phase failed"))
-      else {
-        Future.successful(Ok(Json.toJson(result)))
-      }
-    } else {
-      Future.successful(NotFound("Missing config file"))
-    }
-  }
-
-  @ApiResponses(Array(
-    new ApiResponse(code = 400, message = "Invalid parameters supplied"),
-    new ApiResponse(code = 404, message = "Access purpose not found")))
+    new ApiResponse(code = 500, message = "Error processing result")))
   def getRewrittenQuery = Action.async(parse.json[RequestQuery]) { request =>
 
     val queryObject = request.body
@@ -90,20 +42,16 @@ class EnforcementEngine @Inject() (config: Configuration,  initService: Init) ex
           result = com.ibm.research.storage.policy.enforcement.sql.runtime.SparkSqlRuntime.getNewQuery(spark, configFullPath,purpose, accessType, query, "requester.id", requesterId)
           //result = "OK"
         }catch {
-          case e: Exception => result = ""
+          case e: Exception => result = ""; LOGGER.error("Exception in engine " + e);
         }
-
-
-
-      if (result.equals(""))
-        Future.successful(BadRequest("run time phase failed"))
+      if (result.equals("")) //TODO: make the error message more informative
+        Future.successful(InternalServerError("Failure"))
       else {
-//        println (result)
-        //Future.successful(Ok(Json.toJson(result)))
+        println (result)
         Future.successful(Ok(result))
       }
     } else {
-      Future.successful(NotFound("Missing config file"))
+      Future.successful(BadRequest("Missing config file"))
     }
   }
 }
